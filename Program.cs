fbabed04e1d03d0234f23ebbab997cdfd6408c4f
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-06-06 23:56:23 -0300

    commit create e consulta de produto

--- Program.cs
+++ Program.cs
@@ -5,13 +5,22 @@ using Loja.models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 // Configurar a conexão com o BD
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<LojaDbContext>(options=>options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 2))));
+builder.Services.AddDbContext<LojaDbContext>(options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 2))));
 
 var app = builder.Build();
 
@@ -22,9 +31,26 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAllOrigins");
+
 
 app.MapPost("/createproduto", async (LojaDbContext dbContext, Produto newProduto) => {
-  dbContext.Produtos.Add(newProduto);
-  await dbContext.SaveChangesAsync();
-  return Results.Created($"/createproduto/{newProduto.Id}", newProduto);
+    dbContext.Produtos.Add(newProduto);
+    await dbContext.SaveChangesAsync();
+    return Results.Created($"/createproduto/{newProduto.Id}", newProduto);
 });
+
+app.MapGet("/produtos", async (LojaDbContext dbContext) => {
+  var produtos = await dbContext.Produtos.ToListAsync();
+  return Results.Ok(produtos);
+});
+
+app.MapGet("/produtos/{id}", async (int id, LojaDbContext dbContext) => {
+  var produto = await dbContext.Produtos.FindAsync(id);
+  if (produto == null) {
+    return Results.NotFound($"Produto with ID {id} not found.");
+  }
+  return Results.Ok(produto);
+});
+
+app.Run();
--- obj/Debug/net8.0/Loja.AssemblyInfo.cs
+++ obj/Debug/net8.0/Loja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34a1d534b0f7e282da7d38ff720491d9cd85204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00648bc64f1ad3ee692d7b6c02b47804862e9c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
