37608e833d8551630a72ecd195dc13a5e9adb0ea
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-06-19 23:18:02 -0300

    adicionando as primary key

--- models/Cliente.cs
+++ models/Cliente.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Loja.models{
     public class Cliente{
+
+        [Key]
         public int Id { get; set; }
         public String? Nome { get; set; }
         public String? Cpf { get; set; }
--- models/Produto.cs
+++ models/Produto.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Loja.models
-{
-    public class Produto{
+{       
+        public class Produto{
+            [Key]
         public int Id { get; set; }
         public String? Nome { get; set; }
         public Double Preco { get; set; }
